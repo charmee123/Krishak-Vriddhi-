07b32eca9fa72c1f22ba9077a67a9f52cb4bb778
charmee123 <92216225+charmee123@users.noreply.github.com>
2023-01-28 00:23:33 +0530

    Added files via folder

---
+++ script.js
@@ -0,0 +1,20 @@
+//login page script
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+//sign up page script
+// Get the modal
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
